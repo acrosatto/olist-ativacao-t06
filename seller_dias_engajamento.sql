fdafc107069c8709deae331c0e19d00564ff41e4
acrosatto <actna2000@gmail.com>
2026-05-31 22:53:43 +0000

    Quantidade de dias com vendas e engajamento
    Criação das colunas de dias com vendas total, 28, 56 e 365 dias. Criação da coluna dias sem venda e taxa de engajamento

---
+++ seller_dias_engajamento.sql
@@ -0,0 +1,37 @@
+WITH params AS (
+      SELECT DATE('2018-08-01') AS date_ref
+  ),
+  dias_vida AS (
+        SELECT oi.seller_id,
+            -- Quantidade de dias venda nos diferentes períodos
+            count(DISTINCT date(o.order_purchase_timestamp)) as dias_vendas_total,
+            count(DISTINCT case when date(o.order_purchase_timestamp) >= date_sub(p.date_ref, 28)
+                            AND date(o.order_purchase_timestamp) <= p.date_ref
+                            THEN date(o.order_purchase_timestamp) END) as dias_vendas_D28,
+
+            count(DISTINCT case when date(o.order_purchase_timestamp) >= date_sub(p.date_ref, 56)
+                            AND date(o.order_purchase_timestamp) <= p.date_ref
+                            THEN date(o.order_purchase_timestamp) END) as dias_vendas_D56,
+
+            count(DISTINCT case when date(o.order_purchase_timestamp) >= date_sub(p.date_ref, 365)
+                            AND date(o.order_purchase_timestamp) <= p.date_ref
+                            THEN date(o.order_purchase_timestamp) END) as dias_vendas_D365,
+            datediff(p.date_ref, min(date(o.order_purchase_timestamp))) -
+            count(DISTINCT date(o.order_purchase_timestamp)) as dias_sem_vendas,
+
+            -- dias com venda / dias sem venda na vida total do seller
+            round(coalesce(try_divide(
+            count(DISTINCT date(o.order_purchase_timestamp)),
+
+            datediff(p.date_ref, min(date(o.order_purchase_timestamp))) -
+            count(DISTINCT date(o.order_purchase_timestamp))), 0), 2) as taxa_engajamento
+
+        FROM olist.orders o
+        LEFT JOIN olist.order_items oi 
+        ON o.order_id = oi.order_id
+        CROSS JOIN params p
+        WHERE oi.seller_id IS NOT NULL
+        GROUP BY oi.seller_id, p.date_ref
+        ORDER BY dias_vendas_total DESC)
+
+  SELECT * FROM dias_vida
